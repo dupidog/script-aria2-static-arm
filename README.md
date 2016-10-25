7187e131b06bcff1f2af155d94aee632fb0a79d0
Dupidog.WU <dupidog@gmail.com>
2016-10-25 10:57:08 +0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# aria2-static-crosscompile-for--arm
+# script-aria2-static-arm
+
+One key script to build newest static aria2 for arm machines.
+Only for x86(amd64) debian/ubuntu hosts.
+
+## Usage
+```bash
+git clone https://github.com/dupidog/script-aria2-static-arm.git
+cd script-aria2-static-arm
+./static-aria2-arm.sh
+```
---
+++ static-aria2-arm.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+TOOLCHAINDIR=/opt
+TOOLCHAIN=$TOOLCHAINDIR/arm-2014.05
+PREFIX=$TOOLCHAIN/usr/local
+HOST=arm-none-linux-gnueabi
+
+mkdir -p $TOOLCHAIN
+mkdir -p $PREFIX
+
+echo "Start build static aria2c for arm"
+echo "TOOLCHAIN: $TOOLCHAIN"
+echo "PREFIX:    $PREFIX"
+echo "HOST:      $HOST"
+echo "BUILDROOT: $(pwd)"
+echo
+
+# Install build packages
+apt-get install -y build-essential autoconf autopoint automake lib32z1 git docbook2x krb5-multidev libkrb5-3 pkg-config
+
+# Download and extract crosscompiler
+wget http://dupidog.tk/arm-2014.05-29-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
+tar -jxf arm-2014.05-29-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2i -C $TOOLCHAINDIR
+
+# PATH for crosscompiler
+PATH=$PATH:$TOOLCHAIN/bin/
+
+# Dependence package list
+# 1. c-ares
+# 2. expat
+# 3. zlib
+# 4. openssl
+# 5. libssh2 (not ok yet)
+# 6. sqlite (not ok yet)
+
+# Build and install dependence
+# 1. c-ares
+git clone https://github.com/c-ares/c-ares.git
+cd c-ares
+git checkout –b dev cares-1_12_0
+sed -i 's#\[-\]#[1.12.0]g' configure.ac
+./buildconf
+./configure --prefix=$PREFIX --host=$HOST CC=$HOST-gcc --enable-shared=no --enable-static=yes
+make
+make install
+cd ..
+
+# 2. expat
+git clone https://git.code.sf.net/p/expat/code_git expat
+cd expat
+git checkout –b dev R_2_2_0
+./buildconf.sh
+./configure --prefix=$PREFIX --target=$HOST --host=$HOST CC=$HOST-gcc --enable-shared=no --enable-static=yes
+make
+make install
+cd ..
+
+# 3. zlib
+git clone https://github.com/madler/zlib.git
+cd zlib
+git checkout –b dev v1.2.8
+prefix=$PREFIX AR=$HOST-ar CC=$HOST-gcc CFLAGS="-O4" ./configure --static
+make
+make install
+
+# 4. openssl
+git clone https://github.com/openssl/openssl.git
+cd openssl
+git checkout –b dev OpenSSL-fips-2_0_12
+./Configure --prefix=$PREFIX --openssldir=$PREFIX --with-zlib-lib=$PREFIX/lib --with-zlib-include=$PREFIX/include --cross-compile-prefix=$HOST- linux-armv4
+mv /usr/bin/pod2man /usr/bin/pod2man.bak
+make
+make install
+mv /usr/bin/pod2man.bak /usr/bin/pod2man
+cd ..
+
+# Build aria2
+git clone https://github.com/aria2/aria2.git
+cd aria2
+autoreconf -i
+./configure --host=$HOST --prefix=$PREFIX ARIA2_STATIC=yes CFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib" CPPFLAGS="-I$PREFIX/include"
+make
+make install
+cd ..
+
+# Strip binary
+cp $PREFIX/bin/aria2c .
+$HOST-strip aria2c
+
+echo
+echo "Done!"
